55353bef0194b8979aed120cbc01d8682356d53f
joan <joan.herisson@univ-evry.fr>
2019-09-18 15:19:43 +0200

    first commit

---
+++ code/tool_makeSource.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('Input source molecule')
+    parser.add_argument('-name', type=str)
+    parser.add_argument('-inchi', type=str)
+    parser.add_argument('-sourceFile', type=str)
+    params = parser.parse_args()
+
+    with open(params.sourceFile, 'w') as fi:
+        csv_writer = csv.writer(fi, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['Name', 'InChI'])
+        csv_writer.writerow([params.name, params.inchi])

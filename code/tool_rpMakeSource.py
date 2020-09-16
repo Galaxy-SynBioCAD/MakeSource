8d62835e84ee3fbdf23e52aeb6b32a2a1d13f5ba
mdulac <melchior.dulac@inra.fr>
2020-09-16 14:36:53 +0200

    removing the space

--- code/tool_rpMakeSource.py
+++ code/tool_rpMakeSource.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     with open(params.sourceFile, 'w') as fi:
         csv_writer = csv.writer(fi, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow(['Name', 'InChI'])
-        csv_writer.writerow([params.name, params.inchi])
+        csv_writer.writerow([params.name, params.inchi.replace(' ', '')])
